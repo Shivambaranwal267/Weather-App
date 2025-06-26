164286b97d06f51e51e7ab2451d7b35568ac0c60
SHIVAM BARANWAL <98750358+Shivambaranwal267@users.noreply.github.com>
2025-06-26 20:47:59 +0530

    ui change

--- app/src/main/java/com/example/weatherapp/view/WeatherActivity.java
+++ app/src/main/java/com/example/weatherapp/view/WeatherActivity.java
@@ -1,7 +1,6 @@
 package com.example.weatherapp.view;
 
 import android.annotation.SuppressLint;
-
 import android.content.Context;
 import android.location.LocationListener;
 import android.location.LocationManager;
